5fe7249d8a35336d363423be5b536e2a12030bf1
Dayana Uzunova <daqnauzunova@abv.bg>
2025-05-14 21:42:29 +0300

    Intro to TypeScript.

---
+++ Introduction to TypeScript/Introduction to TypeScript/demo.js
@@ -0,0 +1,4 @@
+var firstname = 'Dayana';
+var lastname = 'Uzunova';
+var myAge = 22;
+console.log("Hello, my name is ".concat(firstname, " ").concat(lastname, " and I am ").concat(myAge, " years old!"));
---
+++ Introduction to TypeScript/Introduction to TypeScript/demo.ts
@@ -0,0 +1,5 @@
+let firstname: string = 'Dayana';
+let lastname: string = 'Uzunova';
+let myAge: number = 22;
+
+console.log(`Hello, my name is ${firstname} ${lastname} and I am ${myAge} years old!`);
