b28ea45ec391f9937c1676ef3c7a032cae03a07c
Dayana Uzunova <daqnauzunova@abv.bg>
2025-05-24 19:40:15 +0300

    Exercises with TypeScript.

---
+++ Exercise - Introduction to TypeScript/07.Unknown Response/07.js
@@ -0,0 +1,9 @@
+function unknownResponse(arg) {
+    if ('value' in arg && typeof arg.value === 'string') {
+        return arg.value;
+    }
+    return '-';
+}
+console.log(unknownResponse({ code: 200, text: 'Ok', value: [1, 2, 3] }));
+console.log(unknownResponse({ code: 301, text: 'Moved Permanently', value: 'New Url' }));
+console.log(unknownResponse({ code: 201, text: 'Created', value: 'Object Created' }));
