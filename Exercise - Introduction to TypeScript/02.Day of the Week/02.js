cc2e9db38eeb68643e5312e420f8be39e089579a
Dayana Uzunova <daqnauzunova@abv.bg>
2025-05-24 19:38:10 +0300

    Exercises with TypeScript.

---
+++ Exercise - Introduction to TypeScript/02.Day of the Week/02.js
@@ -0,0 +1,16 @@
+function dayOfWeek(dayNum) {
+    var Days;
+    (function (Days) {
+        Days[Days["Monday"] = 1] = "Monday";
+        Days[Days["Tuesday"] = 2] = "Tuesday";
+        Days[Days["Wednesday"] = 3] = "Wednesday";
+        Days[Days["Thursday"] = 4] = "Thursday";
+        Days[Days["Friday"] = 5] = "Friday";
+        Days[Days["Saturday"] = 6] = "Saturday";
+        Days[Days["Sunday"] = 7] = "Sunday";
+    })(Days || (Days = {}));
+    console.log(Days[dayNum] || 'error');
+}
+dayOfWeek(1);
+dayOfWeek(5);
+dayOfWeek(9);
