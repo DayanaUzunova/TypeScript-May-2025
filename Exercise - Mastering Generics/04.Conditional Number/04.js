a9ae36f6d7b3d5462b2ccc194f16ea25600039a5
Dayana Uzunova <daqnauzunova@abv.bg>
2025-06-06 20:04:50 +0300

    Generics Exercises.

---
+++ Exercise - Mastering Generics/04.Conditional Number/04.js
@@ -0,0 +1,11 @@
+function conditionalNumber(value) {
+    if (typeof value === 'number') {
+        console.log(value.toFixed(2));
+    }
+    else {
+        console.log(value);
+    }
+}
+conditionalNumber(20.3555);
+conditionalNumber('wow');
+conditionalNumber('a string');
